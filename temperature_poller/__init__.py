c899ec9a118eb2f52707af517423b7d0d33ba8bc
Lavrov Ruslan <130167893+Gugenot2911@users.noreply.github.com>
2026-04-22 11:31:48 +0700

    Create __init__.py

---
+++ temperature_poller/__init__.py
@@ -0,0 +1,6 @@
+from models import HostInfo, TemperatureResponse
+from sqlite_temperature import DatabaseConfig, TemperatureDatabase, TemperatureDBManager
+# from poller import TemperaturePoller
+
+
+__all__ = ['HostInfo', 'TemperatureResponse', 'DatabaseConfig', 'TemperatureDBManager']
